e6be53cfdca4ba931bf01cc82cf9078771396e31
Niaz Bin Siraj <niazbinsiraj@gmail.com>
2019-03-11 20:48:33 +0600

    Base Files Added

---
+++ RunAlexRun/Assets/Scripts/playerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+
+    public float jumpSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void doJump()
+    {
+        if (isDead == false)
+        {
+            Vector2 moveVertical = new Vector2(0.0f, 1.0f);
+            rb2d.AddForce(moveVertical * jumpSpeed);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Princess"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ RunAlexRun/Assets/Scripts/scrollingObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollingObject : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    private float rateOfIncreasingVelocity;
+    private int frameCount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(-1.5f, 0);
+        rateOfIncreasingVelocity = 1.0f;
+        frameCount = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        frameCount++;
+
+        if(frameCount == 700)
+        {
+            frameCount = 0;
+            rateOfIncreasingVelocity += 0.5f;
+            rb2d.velocity = new Vector2(-1.5f * rateOfIncreasingVelocity, 0);
+        }
+    }
+}
